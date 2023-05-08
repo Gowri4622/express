07785f7c884f43f33f0657dda1afb7064073b036
Gowri4622 <gowrihifi@gmail.com>
2023-05-08 18:47:46 +0530

    Created service page.

---
+++ contactus.js
@@ -0,0 +1,9 @@
+const express=require('express')
+const router = express.Router()
+
+router.get('/',(request,response)=>{
+    response.send("This is the Contact Page")
+})
+
+
+module.exports=router
